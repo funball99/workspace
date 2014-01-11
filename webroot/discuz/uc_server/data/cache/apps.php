2786b83781164cbc514542ca50ab99a5181a7872
wilson <281579218@qq.com>
2014-01-11 23:50:02 +0800

    201401112350

---
+++ webroot/discuz/uc_server/data/cache/apps.php
@@ -0,0 +1,23 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => 'Discuz! Board',
+    'url' => 'http://discuz.gz.1251006836.clb.myqcloud.com',
+    'authkey' => 'W6rcge6dqcRa1cg9DdE1TcE4qeH4x1J3v8j452a6z8haVfm7H6m3GckdTfufR8w2',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => '',
+    'tagtemplates' => '',
+    'allowips' => '',
+  ),
+);
+
+?>
